39a861c037d0c94ff3541bb612f6d138c6e71def
Nic Seltzer <nic@nicseltzer.com>
2017-12-26 13:57:19 -0800

    updating and adding git configuration and scripts; dockerfile too

---
+++ backbone/pythia-jenkins/src/main/groovy/SetGitConfig.groovy
@@ -0,0 +1,29 @@
+@Grab('org.yaml:snakeyaml:1.17')
+
+import java.util.logging.Logger
+import jenkins.model.Jenkins
+import org.yaml.snakeyaml.Yaml
+
+Logger logger = Logger.getLogger("")
+Jenkins jenkins = Jenkins.getInstance()
+Yaml yaml = new Yaml()
+
+String configPath = System.getenv("JENKINS_CONFIG_PATH")
+try {
+    configText = new File("${configPath}/git_config.yml").text
+} catch (FileNotFoundException e) {
+    logger.severe("Cannot find config file path @ ${configPath}/git_config.yml")
+    jenkins.doSafeExit(null)
+    System.exit(1)
+}
+
+Map gitConfig = yaml.load(configText)
+
+def gitScm = jenkins.getDescriptorByType(
+                hudson.plugins.git.GitSCM.DescriptorImpl.class
+             )
+
+gitScm.setGlobalConfigName(gitConfig.NAME)
+gitScm.setGlobalConfigEmail(gitConfig.EMAIL)
+
+jenkins.save()
