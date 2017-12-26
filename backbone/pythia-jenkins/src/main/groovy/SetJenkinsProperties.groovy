3559600acb563907c2f86bfcf2740ffe3d03851f
Nic Seltzer <nic@nicseltzer.com>
2017-12-26 14:07:52 -0800

    just added the config file; fast-follow with the script for configuring jenkis properties

---
+++ backbone/pythia-jenkins/src/main/groovy/SetJenkinsProperties.groovy
@@ -0,0 +1,33 @@
+/**
+* set global properties
+*
+* There are a fair amount of properties that can only be set as a runtime flag
+* or via the groovy script console. This script provides unified place to load
+* them
+**/
+
+@Grab('org.yaml:snakeyaml:1.17')
+
+import java.util.logging.Logger
+import hudson.model.*
+import org.yaml.snakeyaml.Yaml
+
+Logger logger = Logger.getLogger("")
+Yaml yaml = new Yaml()
+
+String configPath = System.getenv("JENKINS_CONFIG_PATH")
+String configText
+
+try {
+    configText = new File("${configPath}/properties_config.yml").text
+} catch (FileNotFoundException e) {
+    logger.severe("Cannot find config file path @ ${configPath}/properties_config.yml")
+    jenkins.doSafeExit(null)
+    System.exit(1)
+}
+
+List properties = yaml.load(configText)
+
+properties.each { property ->
+    System.setProperty(property.KEY, property.VALUE)
+}
