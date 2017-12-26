18572d41c5981b542dcc5ea88c6696e9f3a55d90
Nic Seltzer <nic@nicseltzer.com>
2017-12-26 15:05:53 -0800

    added logging and learned about .isQuietingDown()

--- backbone/pythia-jenkins/src/main/groovy/ConfigureCSRF.groovy
+++ backbone/pythia-jenkins/src/main/groovy/ConfigureCSRF.groovy
@@ -1,7 +1,37 @@
+@Grab('org.yaml:snakeyaml:1.17')
+
+import java.util.logging.Logger
 import hudson.security.csrf.DefaultCrumbIssuer
 import jenkins.model.Jenkins
- 
-def jenkins = Jenkins.instance
-jenkins.setCrumbIssuer(new DefaultCrumbIssuer(true))
-jenkins.save()
+import org.yaml.snakeyaml.Yaml
 
+Logger logger = Logger.getLogger("")
+Jenkins jenkins = Jenkins.getInstance()
+Yaml yaml = new Yaml()
+
+if (jenkins.isQuietingDown()) {
+    logger.info('No action taken. Shutdown mode enabled.')
+    System.exit(0)
+} else {
+    String configPath = System.getenv("JENKINS_CONFIG_PATH")
+    String configText
+
+    try {
+        configText = new File("${configPath}/main_config.yml").text
+    } catch (FileNotFoundException e) {
+        logger.severe("Cannot find config file path @ ${configPath}/main_config.yml")
+        jenkins.doSafeExit(null)
+        System.exit(1)
+    }
+
+    Boolean csrfEnabled = (Boolean) yaml.load(configText).CSRF.CSRF_ENABLED
+
+    if (!csrfEnabled) {
+        logger.info("Jenkins CSRF is disabled in ${configPath}/main_config.yml")
+        System.exit(0)
+    }
+
+    logger.info("CSRF has been configured for Jenkins.")
+    jenkins.setCrumbIssuer(new DefaultCrumbIssuer(true))
+    jenkins.save()
+}
